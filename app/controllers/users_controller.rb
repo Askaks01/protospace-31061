94b143dfe31686e30b881ad1c0de2abcf4b51602
Askaks01 <72367357+Askaks01@users.noreply.github.com>
2020-10-23 11:00:42 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+
+    params.require(:user).permit(:name, :profile, :occupation, :position).merge(prototype_id: current_user.id)
+
+  end
+
+end
